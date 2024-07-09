9273f12940e5fade95e4bd4c81c7992bd64386e0
0x7a726f <69540160+0x7a726f@users.noreply.github.com>
2024-07-10 00:54:42 +0300

    Update and rename mnist9969.py to model2.py

--- model2/model2.py
+++ model2/model2.py
@@ -1,7 +1,6 @@
 import numpy as np
 import tensorflow as tf
 
-# Load and normalize data
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = tf.keras.utils.normalize(x_train, axis=1)
@@ -9,7 +8,6 @@ x_test = tf.keras.utils.normalize(x_test, axis=1)
 x_train = x_train.reshape(-1, 28, 28, 1)
 x_test = x_test.reshape(-1, 28, 28, 1)
 
-# Data augmentation
 datagen = tf.keras.preprocessing.image.ImageDataGenerator(
     rotation_range=10,
     zoom_range=0.1,
@@ -22,7 +20,6 @@ y_augmented = np.empty((augment_size,), dtype=np.uint8)
 augment_iterator = datagen.flow(x_train, y_train, batch_size=augment_size, shuffle=False)
 x_augmented, y_augmented = next(augment_iterator)
 
-# Combine and shuffle augmented data
 x_train_combined = np.concatenate((x_train, x_augmented), axis=0)
 y_train_combined = np.concatenate((y_train, y_augmented), axis=0)
 shuffle_indices = np.arange(x_train_combined.shape[0])
@@ -30,7 +27,6 @@ np.random.shuffle(shuffle_indices)
 x_train_combined = x_train_combined[shuffle_indices]
 y_train_combined = y_train_combined[shuffle_indices]
 
-# Model architecture
 model = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
     tf.keras.layers.BatchNormalization(),
@@ -56,15 +52,12 @@ model = tf.keras.models.Sequential([
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-# Callbacks
 early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
 reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=3, min_lr=1e-6)
 model_checkpoint = tf.keras.callbacks.ModelCheckpoint('best_model.h5', save_best_only=True, monitor='val_loss', mode='min')
 
-# Train the model
 history = model.fit(x_train_combined, y_train_combined, epochs=40, validation_data=(x_test, y_test), callbacks=[early_stopping, reduce_lr, model_checkpoint])
 
-# Evaluate the model
 loss, accuracy = model.evaluate(x_test, y_test)
 model.save("cnn_model_with_augmented_data")
 
